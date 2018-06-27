167626222d7f1b52d99818e1626009fc08ac852c
lwesy <lw@esyoil.com>
2018-06-27 15:20:58 +0200

    Use beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,14 +5,16 @@ import App from "../App";
 import CommentBox from "../CommentBox";
 import CommentList from "../CommentList";
 
-it("shows a comment box", () => {
-  const wrapper = shallow(<App />);
+let wrapper;
 
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapper = shallow(<App />);
-
   expect(wrapper.find(CommentList).length).toEqual(1);
 });
