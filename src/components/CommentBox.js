1c45c2ec055640ffe802abe05a2add01a958d26e
lwesy <lw@esyoil.com>
2018-06-28 11:09:12 +0200

    Fetch comments

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -24,13 +24,16 @@ export default connect(
 
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <h4>Add a Comment</h4>
-          <textarea value={this.state.comment} onChange={this.handleInput} />
-          <div>
-            <button>Submit Comment</button>
-          </div>
-        </form>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <h4>Add a Comment</h4>
+            <textarea value={this.state.comment} onChange={this.handleInput} />
+            <div>
+              <button>Submit Comment</button>
+            </div>
+          </form>
+          <button onClick={this.props.fetchComments}>Fetch Comments</button>
+        </div>
       );
     }
   }
