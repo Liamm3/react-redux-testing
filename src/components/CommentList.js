66faaa8c6f58af70497906579a80ebf2125035e5
lwesy <lw@esyoil.com>
2018-06-28 10:13:38 +0200

    Connect to CommentList.js to redux

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,3 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => <div>Comment List</div>;
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+export default connect(mapStateToProps)(
+  class extends Component {
+    renderComments = () =>
+      this.props.comments.map(comment => <li key={comment}>{comment}</li>);
+
+    render() {
+      return (
+        <div>
+          <ul>{this.renderComments()}</ul>
+        </div>
+      );
+    }
+  }
+);
